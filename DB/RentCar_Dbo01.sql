a6dc13e9005bcd4756111f1800330c4b5cde990d
vik37 <59177404+vik37@users.noreply.github.com>
2021-02-18 21:48:55 +0100

    RentACar_db

---
+++ DB/RentCar_Dbo01.sql
@@ -0,0 +1,20 @@
+use [RentaCarDb]
+
+select *
+from [dbo].[AddEquipment]
+select *
+from [dbo].[AirportService]
+select *
+from [dbo].[AspNetRoleClaims]
+select *
+from [dbo].[AspNetRoles]
+select *
+from [dbo].[AspNetUserClaims]
+select *
+from [dbo].[AspNetUserLogins]
+select *
+from [dbo].[AspNetUserRoles]
+select *
+from [dbo].[AspNetUsers]
+select *
+from [dbo].[Vehicle]
